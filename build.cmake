c7336a153e32013161d4ba22a2c8033a4b787cc0
xicilion <lion@9465.net>
2023-12-13 22:19:57 +0800

    build: Add CMake script to display GLIBC information.

---
+++ build.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.0)
+
+get_filename_component(name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+
+include(fib-addon/build_tools/cmake-scripts/get_env.cmake)
+
+set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
+build("${CMAKE_CURRENT_SOURCE_DIR}" "${WORK_ROOT}" "${name}")
+
+set(BIN_PATH "${WORK_ROOT}/bin/${DIST_DIRNAME}")
+
+message("")
+if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux")
+    message("==== GLIBC ====")
+    execute_process(COMMAND objdump ${BIN_PATH}/${name}.node -p COMMAND grep GLIBCX*_[0-9.]* -o COMMAND sort COMMAND uniq)
+elseif(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin")
+    execute_process(COMMAND otool -L ${BIN_PATH}/${name}.node)
+endif()
+message("")
