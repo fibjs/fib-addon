10202e1a13fc16baf3d42670832a2fa85c73a251
xicilion <lion@9465.net>
2024-09-20 13:23:37 +0800

    upgrade build_tools.

--- build.cmake
+++ build.cmake
@@ -4,6 +4,8 @@ get_filename_component(name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 
 include(fib-addon/build_tools/cmake-scripts/get_env.cmake)
 
+set(BUILD_WITH_MSVC "1")
+
 set(WORK_ROOT "${CMAKE_CURRENT_SOURCE_DIR}")
 build("${CMAKE_CURRENT_SOURCE_DIR}" "${WORK_ROOT}" "${name}")
 
